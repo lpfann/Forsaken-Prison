f4f8a1134432185eec8644952d828edf0d625c2b
Lukas Pfannschmidt <Mirek.1337@gmail.com>
2012-08-28 00:29:37 +0200

    " "

---
+++ src/Spiel/model/Entities/monsterpack/EvilMonk.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Spiel.model.Entities.monsterpack;
+
+import Spiel.model.Entities.Monster;
+import Spiel.model.MainModel;
+
+/**
+ *
+ * @author Lukas
+ */
+public class EvilMonk extends Monster {
+
+
+   public EvilMonk(int x1, int y1, int w, int h, MainModel main) {
+      super(0, 0, 40, 5, "Besessener Mönch", 'M', main);
+      this.setstartposition(x1, y1, w, h);
+      setXp(30);
+      setMonsterlvl(4);
+      setSpawnrate(0.05);
+   }
+}
+
---
+++ src/Spiel/model/Entities/monsterpack/Skelett.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Spiel.model.Entities.monsterpack;
+
+import Spiel.model.Entities.Monster;
+import Spiel.model.MainModel;
+
+/**
+ *
+ * @author Lukas
+ */
+public class Skelett extends Monster {
+
+   public Skelett(int x1, int y1, int w, int h, MainModel main) {
+      super(0, 0, 40, 5, "Skelett", 'S', main);
+      this.setstartposition(x1, y1, w, h);
+      setXp(10);
+      setMonsterlvl(2);
+      setSpawnrate(0.3);
+   }
+    }
+
